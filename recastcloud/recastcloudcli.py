370d4b3e374dcbe1782d2ff5113a7e9a4d007559
lukasheinrich <lukas.heinrich@gmail.com>
2017-10-15 20:46:15 -0500

    add cli

---
+++ recastcloud/recastcloudcli.py
@@ -0,0 +1,25 @@
+import yaml
+import base64
+import sys
+import os
+import click
+
+
+@click.group()
+def cloudcli():
+    pass
+
+@cloudcli.command()
+@click.argument('infile')
+@click.argument('filebase')
+@click.argument('outfile')
+def make_helm_values(infile,filebase,outfile):
+    data = yaml.load(open(infile))
+
+    for k,v in data['files'].items():
+        filetoget = os.path.join(filebase,v)
+        print 'getting file {}'.format(filetoget)
+        data['files'][k] = base64.b64encode(open(filetoget).read())
+
+
+    yaml.dump(data,open(outfile,'w'), default_flow_style = False)
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'recast-cloudutils',
+  version = '0.1.0',
+  description = 'cloud deployment tools for RECAST',
+  long_description =  'cloud deployment tools for RECAST',
+  url = 'http://github.com/recast-hep/recast-cloudutils',
+  author = 'Lukas Heinrich',
+  author_email = 'lukas.heinrich@cern.ch',
+  packages=find_packages(),
+  install_requires = [
+    'Click',
+  ],
+  entry_points = {
+    'console_scripts': [
+      'recast-cloud = recastcloud.recastcloudcli:cloudcli',
+    ]
+  },
+  include_package_data = True,
+  zip_safe=False
+)
