a8f17703a5a0d4d560c0f743198f84d0feee71a2
lukasheinrich <lukas.heinrich@gmail.com>
2018-03-30 21:07:33 +0200

    more dev

--- recastcloud/recastcloudcli.py
+++ recastcloud/recastcloudcli.py
@@ -12,14 +12,16 @@ def cloudcli():
 @cloudcli.command()
 @click.argument('infile')
 @click.argument('filebase')
-@click.argument('outfile')
+@click.option('-o','--outfile', default=None)
 def make_helm_values(infile,filebase,outfile):
     data = yaml.load(open(infile))
 
     for k,v in data['files'].items():
         filetoget = os.path.join(filebase,v)
-        click.secho('getting file {}'.format(filetoget))
-        data['files'][k] = base64.b64encode(open(filetoget,'rb').read())
+        data['files'][k] = base64.b64encode(open(filetoget,'rb').read()).decode('utf-8')
 
 
-    yaml.dump(data,open(outfile,'w'), default_flow_style = False)
+    if outfile:
+        yaml.dump(data,open(outfile,'w'), default_flow_style = False)
+    else:
+        print(yaml.dump(data))
